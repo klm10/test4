95c135be9be931e37518b7d63c2afa3719e209e9
Ivan <klim9@bk.ru>
2017-06-06 18:58:15 +0300

    start commit

---
+++ app/src/main/java/ru/bk/klim9/testrebaseapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.bk.klim9.testrebaseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
